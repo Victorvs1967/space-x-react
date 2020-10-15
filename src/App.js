c629b0e1c943943fce8e29c47e084b7e56fa39fb
Victor <victorsmirnov@me.com>
2020-10-15 17:21:59 +0300

    add links to footer

--- src/App.js
+++ src/App.js
@@ -3,20 +3,57 @@ import React from 'react';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Features from './components/features/Features';
-import Calendar from './components/calendar/Calendar';
 import Footer from './components/footer/Footer';
+import FetchData from './service/FetchData'
+
 import './style.css';
 
-function App() {
-  return (
-    <>
-      <Header/>
-      <Main/>
-      <Features/>
-      <Calendar/>
-      <Footer/>
-    </>
-);
+class App extends React.Component {
+
+  fetchData = new FetchData();
+
+  state = {
+    rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: [],
+    links: {},
+  };
+
+  componentDidMount() {
+    this.fetchData.getRocket()
+    .then(data => {
+      this.setState({ rockets: data.map(item => item.name) });
+      return data;
+    })
+    .then(data => data.find(item => item.name === this.state.rocket))
+    .then(rocketFeatures => this.setState({ rocketFeatures }));
+
+    this.fetchData.getCompany()
+    .then(data => this.setState({ links: data.links }));
+  };
+
+  updateRocket() {
+    this.fetchData.getRocket()
+    .then(data => data.find(item => item.name === this.state.rocket))
+    .then(rocketFeatures => this.setState({ rocketFeatures }));
+  }
+
+  changeRocket = rocket => {
+    this.setState({
+      rocket
+    }, this.updateRocket);
+  }
+
+  render() {
+    return (
+      <>
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket} />
+        <Main rocket={this.state.rocket} />
+        <Features rocket={this.state.rocket} rocketFeatures={this.state.rocketFeatures} />
+        <Footer links={this.state.links} />
+      </>
+    );  
+  }
 }
 
 export default App;
--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import RelaxWrapper from 'react-rellax-wrapper';
 
 import './features.css';
 
-const Features = () => (
+const rockets = {
+	'Falcon 1': './img/falcon-1.png',
+	'Falcon 9': './img/falcon-9.png',
+	'Falcon Heavy': './img/falcon-heavy.png',
+	other: './img/starship.png',  
+}
+
+const Features = (props) => (
 	<section className="features">
 		<h2 className="features-title">Falcon 1 <br/>Overview</h2>
 		<div className="overview">
@@ -12,27 +20,29 @@ const Features = () => (
 				<thead>
 					<tr>
 						<td className="table-column">HEIGHT</td>
-						<td className="table-column">22.25 m / 73 ft</td>
+						<td className="table-column">{ props.rocketFeatures ? props.rocketFeatures.height.meters : '22.25'} m / { props.rocketFeatures ? props.rocketFeatures.height.feet : '73'} ft</td>
 					</tr>
 					<tr>
 						<td className="table-column">DIAMETER</td>
-						<td className="table-column">1.68 m / 5.5 ft</td>
+						<td className="table-column">{ props.rocketFeatures ? props.rocketFeatures.diameter.meters : '1.68 '} m / { props.rocketFeatures ? props.rocketFeatures.diameter.feet : '5.5'} ft</td>
 					</tr>
 					<tr>
 						<td className="table-column">MASS</td>
-						<td className="table-column">30,146 kg / 66,460 lb</td>
+						<td className="table-column">{ props.rocketFeatures ? props.rocketFeatures.mass.kg : '30,146'} kg / { props.rocketFeatures ? props.rocketFeatures.mass.lb : '66,460'} lb</td>
 					</tr>
 					<tr>
 						<td className="table-column">PAYLOAD TO LEO</td>
-						<td className="table-column">450 kg / 992 lb</td>
+						<td className="table-column">{ props.rocketFeatures ? props.rocketFeatures.payload_weights[0].kg : '450'} kg / { props.rocketFeatures ? props.rocketFeatures.payload_weights[0].lb : '992'} lb</td>
 					</tr>
 				</thead>
 			</table>
-			<img src="./img/falcon-1.png" alt="rocket" className="rocket" data-rellax-speed="14"/>
+			<RelaxWrapper speed={8}>
+			<img src={rockets[props.rocket] ? rockets[props.rocket] : rockets.other} alt="rocket" className="rocket" data-rellax-speed="14"/>
+			</RelaxWrapper>
 			<article>
 				<h3 className="features-subtitle">DESCRIPTION</h3>
 				<p className="features-text">
-					The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.
+					{ props.rocketFeatures ? props.rocketFeatures.description : 'The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.'}
 				</p>
 			</article>
 		</div>
